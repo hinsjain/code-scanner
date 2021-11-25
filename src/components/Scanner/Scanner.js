043eb496a0dd73bb04ec55975fad58b6ae4c5507
hinsjain <hinsjain8983@gmail.com>
2021-11-25 19:47:19 +0530

    Update Scanner.js

--- src/components/Scanner/Scanner.js
+++ src/components/Scanner/Scanner.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Container, Card, CardContent, Grid} from '@material-ui/core';
+import QrReader from 'react-qr-reader';
 
-function Scanner() {
-    return (
-        <div>
-            Scanner
-        </div>
-    )
+
+function Scanner() { 
+  const [scanResultWebCam, setScanResultWebCam] =  useState('');
+
+  const handleErrorWebCam = (error) => {
+    console.log(error);
+  }
+  const handleScanWebCam = (result) => {
+    if (result){
+        setScanResultWebCam(result);
+    }
+   }
+  return (
+    <Container className="conatiner">
+          <Card>
+              <h2 className="title">Generate Download & Scan QR Code with React js</h2>
+              <CardContent>
+                  <Grid container spacing={2}>
+                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                         <h3>Qr Code Scan by Web Cam</h3>
+                         <QrReader
+                         delay={300}
+                         style={{width: '100%'}}
+                         onError={handleErrorWebCam}
+                         onScan={handleScanWebCam}
+                         />
+                         <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
+                      </Grid>
+                  </Grid>
+              </CardContent>
+          </Card>
+    </Container>
+  );
 }
 
-export default Scanner
+
+export default Scanner;

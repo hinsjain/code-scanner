3cc420e04ba6ad226611ac574d8eee3d95f7703a
hinsjain <hinsjain8983@gmail.com>
2021-11-26 01:30:07 +0530

    Update CsvReader.js

--- src/components/Upload/CsvReader.js
+++ src/components/Upload/CsvReader.js
@@ -1,81 +1,76 @@
-import { Table, TableBody, TableCell, TableRow } from "@material-ui/core";
+import { Button, FormControl } from "@material-ui/core";
 import { useState } from "react";
 import GenerateCode from "./GenerateCode";
+import TableData from "./TableData";
+import './CsvReader.css'
 
 function CsvReader() {
-	const [csvFile, setCsvFile] = useState();
-	const [csvArray, setCsvArray] = useState([]);
+  const [csvFile, setCsvFile] = useState();
+  const [csvArray, setCsvArray] = useState([]);
 
-	const processCSV = (str, delim = ",") => {
-		const headers = str.slice(0, str.indexOf("\n")).split(delim);
-		const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+  const processCSV = (str, delim = ",") => {
+    const headers = str.slice(0, str.indexOf("\n")).split(delim);
+    const rows = str.slice(str.indexOf("\n") + 1).split("\n");
 
-		const newArray = rows.map((row) => {
-			const values = row.split(delim);
-			const eachObject = headers.reduce((obj, header, i) => {
-				obj[header] = values[i];
-				return obj;
-			}, {});
-			return eachObject;
-		});
+    const newArray = rows.map((row) => {
+      const values = row.split(delim);
+      const eachObject = headers.reduce((obj, header, i) => {
+        obj[header] = values[i];
+        return obj;
+      }, {});
+      return eachObject;
+    });
 
-		setCsvArray(newArray);
-	};
+    setCsvArray(newArray);
+  };
 
-	const submit = () => {
-		const file = csvFile;
-		const reader = new FileReader();
+  const submit = () => {
+    const file = csvFile;
+    const reader = new FileReader();
 
-		reader.onload = function (e) {
-			const text = e.target.result;
-			processCSV(text);
-		};
+    reader.onload = function (e) {
+      const text = e.target.result;
+      processCSV(text);
+    };
 
-		reader.readAsText(file);
-	};
+    reader.readAsText(file);
+  };
 
-	return (
-		<form id="csv-form">
-			<input
-				type="file"
-				accept=".csv"
-				id="csvFile"
-				onChange={(e) => {
-					setCsvFile(e.target.files[0]);
-				}}
-			></input>
-			<br />
-			<button
-				onClick={(e) => {
-					e.preventDefault();
-					if (csvFile) submit();
-				}}
-			>
-				Submit
-			</button>
+  return (
+    <div  className="csvReader">
+      <div>
+        <FormControl className="csvReader__form">
+          <p>Please upload CSV file</p>
+          <input
+            type="file"
+            accept=".csv"
+            id="csvFile"
+            onChange={(e) => {
+              setCsvFile(e.target.files[0]);
+            }}
+          ></input>
+          <br />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={!csvFile}
+            onClick={(e) => {
+              if (csvFile) submit();
+            }}
+          >
+            Submit
+          </Button>
+			<br/>
+          <GenerateCode nameList={csvArray} />
+        </FormControl>
+      </div>
 
-            {csvArray.length > 0 ? (
-				<>
-					<Table>
-						<TableBody>
-                                <TableRow>
-									<TableCell>First Name</TableCell>
-									<TableCell>Last name</TableCell>
-								</TableRow>
-							{csvArray.map((item, i) => (
-								<TableRow key={i}>
-									<TableCell>{item.firstName}</TableCell>
-									<TableCell>{item.lastname}</TableCell>
-								</TableRow>
-							))}
-						</TableBody>
-					</Table>
-				</>
-			) : null}
-
-			<GenerateCode nameList={csvArray} />
-		</form>
-	);
+      <div className="csvReader__table">
+        {csvArray.length > 0 ? <TableData data={csvArray} /> : null}
+      </div>
+    </div>
+  );
 }
 
 export default CsvReader;

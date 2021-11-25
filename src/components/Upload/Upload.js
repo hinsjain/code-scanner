fa02ec7d00481c6f514fef8c48167b3d995e3a48
hinsjain <hinsjain8983@gmail.com>
2021-11-25 19:47:13 +0530

    Update Upload.js

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,14 +1,18 @@
-import { Button, Card } from '@material-ui/core'
+import { Card } from '@material-ui/core'
 import React from 'react'
+import CsvReader from './CsvReader'
 import './Upload.css'
 
 function Upload() {
     return (
         <Card variant = "elevation" className="upload">
-            <p>Please upload excel</p>
+            {/* <p>Please upload excel</p>
             <input className="upload__select" type="file" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
             <br/>
-            <Button type="submit" className="upload__button" variant="contained" color = "primary" >Upload</Button>
+            <Button type="submit" className="upload__button" variant="contained" color = "primary" >Upload</Button> */}
+
+            <CsvReader />
+            
         </Card>
     )
 }

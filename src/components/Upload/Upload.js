8a61018d6d8905e64aa3ab54a311f3e20ed5e31c
hinsjain <hinsjain8983@gmail.com>
2021-11-26 01:29:52 +0530

    Update Upload.js

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -6,13 +6,7 @@ import './Upload.css'
 function Upload() {
     return (
         <Card variant = "elevation" className="upload">
-            {/* <p>Please upload excel</p>
-            <input className="upload__select" type="file" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
-            <br/>
-            <Button type="submit" className="upload__button" variant="contained" color = "primary" >Upload</Button> */}
-
-            <CsvReader />
-            
+            <CsvReader />            
         </Card>
     )
 }

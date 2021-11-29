893eecac7e71361441eea098595c7314d96ede14
hinsjain <hinsjain8983@gmail.com>
2021-11-30 01:53:51 +0530

    Update Scanner.js

--- src/components/Scanner/Scanner.js
+++ src/components/Scanner/Scanner.js
@@ -7,56 +7,48 @@ import axios from "axios";
 const baseURL = "http://localhost:3001/users/list";
 
 function Scanner() {
-	const [scanResultWebCam, setScanResultWebCam] = useState("");
-	const [result, setResult] = useState("");
+  const [scanResultWebCam, setScanResultWebCam] = useState("");
+  const [result, setResult] = useState("");
 
-	const [userList, setUserList] = useState([]);
+  const [userList, setUserList] = useState([]);
 
-	useEffect(() => {
-		axios.get(baseURL).then((response) => {
-			const resultData = response.data.data;
-            setUserList(resultData)
-		});
-	}, []);
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      const resultData = response.data.data;
+      setUserList(resultData);
+    });
+  }, []);
 
-	const handleErrorWebCam = (error) => {
-		console.log(error);
-	};
-	const handleScanWebCam = (result) => {
-		if (result) {
-			setScanResultWebCam(result);
+  const handleErrorWebCam = (error) => {
+    console.log(error);
+  };
 
-            userList.map( user => {
-                const name = user.full_name;                
-                const res = name.localeCompare(result);
-
-                console.log(res)
-                if(res){
-                    setResult(`${scanResultWebCam}: Verified`)
-                }
-                else {
-                    setResult(`${scanResultWebCam}: Not found`)
-                }
-		    })
+  const handleScanWebCam = (result) => {
+    if (result) {
+      userList.map((user) => {
+        if (parseInt(user.qr_code) === parseInt(result)) {
+          setScanResultWebCam(`${user.full_name}`)
         }
-	};	
+      });
+    }
+  };
 
-	return (
-		<Card className="container">
-			<h4 className="scanner_title">Scan QR Code</h4>
-			<CardContent className="scanner__cardContent">
-				<Grid>
-					<QrReader
-						className="qrReader"
-						delay={300}
-						onError={handleErrorWebCam}
-						onScan={handleScanWebCam}
-					/>
-					<h3>{result}</h3>
-				</Grid>
-			</CardContent>
-		</Card>
-	);
+  return (
+    <Card className="container">
+      <h4 className="scanner_title">Scan QR Code</h4>
+      <CardContent className="scanner__cardContent">
+        <Grid>
+          <QrReader
+            className="qrReader"
+            delay={300}
+            onError={handleErrorWebCam}
+            onScan={handleScanWebCam}
+          />
+          <h3>{scanResultWebCam ? `${scanResultWebCam}: Verified` : `Not found`}</h3>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default Scanner;

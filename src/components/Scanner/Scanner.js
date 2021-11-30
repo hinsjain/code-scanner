7e963e07f3409108771d901cf6f57193a9d00133
hinsjain <hinsjain8983@gmail.com>
2021-12-01 01:22:00 +0530

    Update Scanner.js

--- src/components/Scanner/Scanner.js
+++ src/components/Scanner/Scanner.js
@@ -7,103 +7,76 @@ import axios from "axios";
 const baseURL = "http://localhost:3001/users";
 
 function Scanner() {
-	const [scanResultWebCam, setScanResultWebCam] = useState("");
-	// const [result1, setResult1] = useState("");
+  const [scanResultWebCam, setScanResultWebCam] = useState("");
 
-	// const [post, setPost] = useState("");
+  const [userList, setUserList] = useState([]);
 
-	const [userList, setUserList] = useState([]);
+  useEffect(() => {
+    axios.get(`${baseURL}/list`).then((response) => {
+      const resultData = response.data.data;
+      setUserList(resultData);
+    });
+  }, []);
 
-	useEffect(() => {
-		axios.get(`${baseURL}/list`).then((response) => {
-			const resultData = response.data.data;
-			setUserList(resultData);
-		});
-	}, []);
+  const handleErrorWebCam = (error) => {
+    console.log(error);
+  };
 
-	const handleScanResult = (userData) => {
-		axios.get(`${baseURL}/tracker_list`).then((response) => {
-			const tracker_list = response.data.data;
-            console.log(tracker_list)
-			tracker_list.map((list) => {
-				switch (parseInt(list.user_id) === parseInt(userData.id)) {
-					case true:
-						setScanResultWebCam(`${userData.full_name}: Already Verified`);
-						break;
-                    
-					case false:
-						axios.post(`${baseURL}/tracker_new`, {
-							user_id: userData.id,
-							verified: 1,
-						});
+  const handleResult = (output) => {
+    //To check if user is present in the created user's QR
+    const scannedUserDetails = userList.map((user) => {
+      if (parseInt(user.qr_code) === parseInt(output)) {
+        return user;
+      } else {
+        return null;
+      }
+    });
 
-						setScanResultWebCam(`${userData.full_name}: Verified`);
-						break;
-                    
-                    default: break;
-				}
-			});
-		});
-	};
+    scannedUserDetails.map((user) => {
+      if (user !== null) {
+        //To check if user is verified or not
+        axios.get(`${baseURL}/tracker_list/${user.id}`).then((response) => {
+          const trackerList = response.data.data;
 
-	const handleErrorWebCam = (error) => {
-		console.log(error);
-	};
+          if (trackerList.length > 0) {
+            setScanResultWebCam(`${user.full_name}: Already Verified`);
+          } else {
+            axios.post(`${baseURL}/tracker_new`, {
+              user_id: user.id,
+              verified: 1,
+            });
+            setScanResultWebCam(`${user.full_name}: Verified`);
+          }
+        });
+      } else {
+        setScanResultWebCam(`Not found`); //Not present in the QR created list
+      }
+    });
+  };
 
-    // const handleResult = (output) => {
-    //     const result1 = userList.map(user => {
-    //         if(parseInt(user.qr_code) === parseInt(output)){
-    //            return user.full_name                    
-    //         }            
-    //         else {
-    //             return null
-    //         }            
-    //     })
+  const handleScanWebCam = (result) => {
+    setScanResultWebCam("");
+    if (result) {
+      handleResult(result);
+    }
+  };
 
-    //     console.log(result1)
-    // }
-
-	const handleScanWebCam = (result) => {
-		setScanResultWebCam("");
-		if (result) {
-            let userFound = {};
-            let flag = false
-			userList.map((user) => {
-				if (parseInt(user.qr_code) === parseInt(result)) {
-					userFound = user
-                    flag = true
-                    //handleScanResult(userFound);
-				} 
-                // else {
-				// 	console.log(userList);
-				// 	setScanResultWebCam("Not found");
-				// }
-			});
-
-            if(flag){
-                handleScanResult(userFound);
-            }
-            
-            // setScanResultWebCam(handleResult(result))
-		}
-	};
-
-	return (
-		<Card className="container">
-			<h4 className="scanner_title">Scan QR Code</h4>
-			<CardContent className="scanner__cardContent">
-				<Grid>
-					<QrReader
-						className="qrReader"
-						delay={500}
-						onError={handleErrorWebCam}
-						onScan={handleScanWebCam}
-					/>
-					<h3>{scanResultWebCam}</h3>
-				</Grid>
-			</CardContent>
-		</Card>
-	);
+  return (
+    <Card className="container">
+      <h4 className="scanner_title">Scan QR Code</h4>
+      <CardContent className="scanner__cardContent">
+        <Grid>
+          <QrReader
+            className="qrReader"
+            delay={1000}
+            onError={handleErrorWebCam}
+            onScan={handleScanWebCam}
+          />
+          <h3>{scanResultWebCam}</h3>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default Scanner;

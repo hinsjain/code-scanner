241162eeedeee6ab3fe4e0ff22d6a3ceb839be69
hinsjain <hinsjain8983@gmail.com>
2021-11-30 19:47:04 +0530

    Update Scanner.js

--- src/components/Scanner/Scanner.js
+++ src/components/Scanner/Scanner.js
@@ -4,51 +4,106 @@ import QrReader from "react-qr-reader";
 import "./Scanner.css";
 import axios from "axios";
 
-const baseURL = "http://localhost:3001/users/list";
+const baseURL = "http://localhost:3001/users";
 
 function Scanner() {
-  const [scanResultWebCam, setScanResultWebCam] = useState("");
-  const [result, setResult] = useState("");
+	const [scanResultWebCam, setScanResultWebCam] = useState("");
+	// const [result1, setResult1] = useState("");
 
-  const [userList, setUserList] = useState([]);
+	// const [post, setPost] = useState("");
 
-  useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      const resultData = response.data.data;
-      setUserList(resultData);
-    });
-  }, []);
+	const [userList, setUserList] = useState([]);
 
-  const handleErrorWebCam = (error) => {
-    console.log(error);
-  };
+	useEffect(() => {
+		axios.get(`${baseURL}/list`).then((response) => {
+			const resultData = response.data.data;
+			setUserList(resultData);
+		});
+	}, []);
 
-  const handleScanWebCam = (result) => {
-    if (result) {
-      userList.map((user) => {
-        if (parseInt(user.qr_code) === parseInt(result)) {
-          setScanResultWebCam(`${user.full_name}`)
-        }
-      });
-    }
-  };
+	const handleScanResult = (userData) => {
+		axios.get(`${baseURL}/tracker_list`).then((response) => {
+			const tracker_list = response.data.data;
+            console.log(tracker_list)
+			tracker_list.map((list) => {
+				switch (parseInt(list.user_id) === parseInt(userData.id)) {
+					case true:
+						setScanResultWebCam(`${userData.full_name}: Already Verified`);
+						break;
+                    
+					case false:
+						axios.post(`${baseURL}/tracker_new`, {
+							user_id: userData.id,
+							verified: 1,
+						});
 
-  return (
-    <Card className="container">
-      <h4 className="scanner_title">Scan QR Code</h4>
-      <CardContent className="scanner__cardContent">
-        <Grid>
-          <QrReader
-            className="qrReader"
-            delay={300}
-            onError={handleErrorWebCam}
-            onScan={handleScanWebCam}
-          />
-          <h3>{scanResultWebCam ? `${scanResultWebCam}: Verified` : `Not found`}</h3>
-        </Grid>
-      </CardContent>
-    </Card>
-  );
+						setScanResultWebCam(`${userData.full_name}: Verified`);
+						break;
+                    
+                    default: break;
+				}
+			});
+		});
+	};
+
+	const handleErrorWebCam = (error) => {
+		console.log(error);
+	};
+
+    // const handleResult = (output) => {
+    //     const result1 = userList.map(user => {
+    //         if(parseInt(user.qr_code) === parseInt(output)){
+    //            return user.full_name                    
+    //         }            
+    //         else {
+    //             return null
+    //         }            
+    //     })
+
+    //     console.log(result1)
+    // }
+
+	const handleScanWebCam = (result) => {
+		setScanResultWebCam("");
+		if (result) {
+            let userFound = {};
+            let flag = false
+			userList.map((user) => {
+				if (parseInt(user.qr_code) === parseInt(result)) {
+					userFound = user
+                    flag = true
+                    //handleScanResult(userFound);
+				} 
+                // else {
+				// 	console.log(userList);
+				// 	setScanResultWebCam("Not found");
+				// }
+			});
+
+            if(flag){
+                handleScanResult(userFound);
+            }
+            
+            // setScanResultWebCam(handleResult(result))
+		}
+	};
+
+	return (
+		<Card className="container">
+			<h4 className="scanner_title">Scan QR Code</h4>
+			<CardContent className="scanner__cardContent">
+				<Grid>
+					<QrReader
+						className="qrReader"
+						delay={500}
+						onError={handleErrorWebCam}
+						onScan={handleScanWebCam}
+					/>
+					<h3>{scanResultWebCam}</h3>
+				</Grid>
+			</CardContent>
+		</Card>
+	);
 }
 
 export default Scanner;

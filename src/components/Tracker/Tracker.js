a2b128d5d16b09628261ae5df8edaed33392ecac
hinsjain <hinsjain8983@gmail.com>
2021-12-01 18:08:18 +0530

    Update Tracker.js

--- src/components/Tracker/Tracker.js
+++ src/components/Tracker/Tracker.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableRow,
+} from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./Tracker.css";
+
+const baseURL = "http://localhost:3001/users";
 
 function Tracker() {
-    return (
-        <div>
-            Tracker
-        </div>
-    )
+	const [verifiedUser, setVerifiedUser] = useState([]);
+
+	useEffect(() => {
+		axios.get(`${baseURL}/verified_list`).then((response) => {
+			const result = response.data.data;
+			setVerifiedUser(result);
+		});
+	}, []);
+
+	const generateTime = (time) => {
+		let date = new Date(time);
+		const newTime = date.toLocaleTimeString();
+		return newTime;
+	};
+
+	return (
+		<div className="tracker">
+			<Table className="tracker__table">
+				<TableHead>
+					<TableRow>
+						<TableCell className="tracker__cell">Name</TableCell>
+						<TableCell className="tracker__cell">Arrival Time</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{verifiedUser.map((user, i) => (
+						<TableRow key={i}>
+							<TableCell className="tracker__bodyCell">
+								{user.full_name}
+							</TableCell>
+							<TableCell>{generateTime(user.time_created)}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</div>
+	);
 }
 
-export default Tracker
+export default Tracker;
